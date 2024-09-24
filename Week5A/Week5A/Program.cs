d560010efea03d16c580f901a1a511b9cd60cc61
Jin <jyi2cute@gmail.com>
2024-09-24 16:52:15 -0400

    initialized code/demo

---
+++ Week5A/Week5A/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Week5A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int myFavNum = 2;
+            int theBestNum = 10;
+
+            int sum;
+
+            sum = myFavNum + theBestNum;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
